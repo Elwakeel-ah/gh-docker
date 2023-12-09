d3f3a66505a7d6794f2f26c82dec8f4ef5096a39
Ahmed Hassan <elwakeel-ah@gmail.com>
2023-12-09 17:48:37 +0200

    add dockerfile3

--- index.js
+++ index.js
@@ -5,10 +5,4 @@ dotenv.config();
 const app = express();
 const port = 3000; //process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.json({ message: "hello from the server" });
-});
-
-app.listen(port, () => {
-  console.log(`server is up and running on port ${port}`);
-});
+console.log(port);
